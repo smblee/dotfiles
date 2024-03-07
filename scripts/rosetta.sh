e2dccc64efc6acbe4aa1894b0290497fde769ed2
Bryan Lee <smblee14@gmail.com>
2024-03-07 13:52:41 -0500

    update

--- scripts/rosetta.sh
+++ scripts/rosetta.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-/usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
-
+if [[ $(uname -m) != "arm64" ]] ; then
+    echo "not arm64"
+else
+    if ! (arch -arch x86_64 uname -m > /dev/null) ; then
+        echo "arm64: no Rosetta installed"
+        softwareupdate --install-rosetta --agree-to-license
+    else
+        echo "arm64: Rosetta already installed"
+    fi
+fi
